98c041e7b5ac24e145551c38450c7438244c5548
Lisa Nguyen <lisan0182@att.net>
2020-01-08 21:10:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0
-  board.each do |token|
-    if space == "X" || token == "O"
+  board.each do |space|
+    if space == "X" || space == "O"
       turns += 1
     end
   end
